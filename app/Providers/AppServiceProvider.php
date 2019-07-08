6429808c637f9e99eed1b719644462d86f928341
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-07-08 10:52:54 -0400

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if(env('REDIRECT_HTTPS'))
+        {
+            $url->forceSchema('https');
+        }
     }
 }
