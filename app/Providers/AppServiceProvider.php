15f4cc1108c93c5acfa60bb3ecf84f1d1928f473
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-07-08 10:54:03 -0400

    use existing APP_ENV variable

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(env('REDIRECT_HTTPS'))
+        if(env('APP_ENV') == 'production')
         {
             $url->forceSchema('https');
         }
