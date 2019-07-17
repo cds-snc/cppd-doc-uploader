abe345e1dc8af239bc213fe4ad72e7888a33d315
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2019-07-17 16:19:46 -0400

    add support for localization

--- routes/web.php
+++ routes/web.php
@@ -13,71 +13,74 @@ use Carbon\Carbon;
 |
 */
 
-Route::get('/', function () {
-    return view('upload');
-});
+Route::group(['prefix' => LaravelLocalization::setLocale()], function()
+{
+    Route::get('/', function () {
+        return view('upload');
+    });
 
-Route::post('/upload', function() {
+    Route::post('/upload', function() {
 
-    request()->validate([
-        'document' => 'required|mimetypes:application/pdf,max:10000'
-    ]);
+        request()->validate([
+            'document' => 'required|mimetypes:application/pdf,max:10000'
+        ]);
 
-    if (request()->hasFile('document')) {
-        if (request()->file('document')->isValid()) {
-            $file = request()->file('document');
-            $path = request()->document->store('uploads');
+        if (request()->hasFile('document')) {
+            if (request()->file('document')->isValid()) {
+                $file = request()->file('document');
+                $path = request()->document->store('uploads');
+                
+                $files = Storage::disk('uploads')->files();
+
+                return response()->json([
+                    'id' => count($files),
+                    'originalName' => $file->getClientOriginalName(),
+                    'fileName' => $file->getClientOriginalName(),
+                    'url' => '/files/' . $file->getClientOriginalName()
+                ]);
+            }
             
-            $files = Storage::disk('uploads')->files();
-
             return response()->json([
-                'id' => count($files),
-                'originalName' => $file->getClientOriginalName(),
-                'fileName' => $file->getClientOriginalName(),
-                'url' => '/files/' . $file->getClientOriginalName()
+                'error' => 'There was some other error'
             ]);
         }
-        
         return response()->json([
-            'error' => 'There was some other error'
+            'error' => 'No file was uploaded'
         ]);
-    }
-    return response()->json([
-        'error' => 'No file was uploaded'
-    ]);
-});
+    });
 
-Route::get('/files', function () {
-    $files = Storage::disk('uploads')->files();
-    $data = [];
+    Route::get('/files', function () {
+        $files = Storage::disk('uploads')->files();
+        $data = [];
 
-    foreach ($files as $file) {
-        array_push($data, [
-            'name' => $file,
-            'url' => '/files/' . $file,
-            'date' => Carbon::createFromTimestamp(Storage::disk('uploads')->lastModified($file))->toDateTimeString()
-        ]);
-    }
+        foreach ($files as $file) {
+            array_push($data, [
+                'name' => $file,
+                'url' => '/files/' . $file,
+                'date' => Carbon::createFromTimestamp(Storage::disk('uploads')->lastModified($file))->toDateTimeString()
+            ]);
+        }
 
-    return view('files', ['files' => $data]);
-});
+        return view('files', ['files' => $data]);
+    });
 
-Route::get('/files/{file}', function($file) {
-    if (Storage::disk('uploads')->exists($file)) {
-        return Storage::disk('uploads')->download($file);
-    }
-
-    return response()->json([
-        'error' => 'File missing'
-    ]);
-});
-
-Route::delete('/files/{file}', function($file) {
-    if (Storage::disk('uploads')->exists($file)) {
-        Storage::disk('uploads')->delete($file);
+    Route::get('/files/{file}', function($file) {
+        if (Storage::disk('uploads')->exists($file)) {
+            return Storage::disk('uploads')->download($file);
+        }
 
         return response()->json([
-            'success'
+            'error' => 'File missing'
         ]);
-    }
+    });
+
+    Route::delete('/files/{file}', function($file) {
+        if (Storage::disk('uploads')->exists($file)) {
+            Storage::disk('uploads')->delete($file);
+
+            return response()->json([
+                'success'
+            ]);
+        }
+    });
 });
